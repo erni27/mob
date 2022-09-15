c2ce7a851a2e396e5962bc3aab0f5d049652474f
Ernest Nguyen <58267404+erni27@users.noreply.github.com>
2022-09-15 18:49:26 +0200

    Assign returned value to var (#9)
    * Assign returned value to var
    
    * Handle notify err

--- mediator_benchmark_test.go
+++ mediator_benchmark_test.go
@@ -8,15 +8,25 @@ import (
 //nolint:errcheck
 func BenchmarkSend(b *testing.B) {
 	defer clearRequestHandlers()
-	handler := &DummyRequestHandler2{}
-	err := RegisterRequestHandler[DummyRequest2, DummyResponse2](handler)
-	if err != nil {
+	if err := RegisterRequestHandler[DummyRequest1, DummyResponse1](&DummyRequestHandler1{}); err != nil {
 		b.Fatalf("register request handler: %v", err)
 	}
-	b.ResetTimer()
+	if err := RegisterRequestHandler[DummyRequest2, DummyResponse2](&DummyRequestHandler2{}); err != nil {
+		b.Fatalf("register request handler: %v", err)
+	}
+	if err := RegisterRequestHandler[DummyRequest1, DummyResponse2](&DummyRequestHandler3{}); err != nil {
+		b.Fatalf("register request handler: %v", err)
+	}
 	ctx := context.Background()
 	req := DummyRequest2{Int: 997}
+	b.ResetTimer()
 	for n := 0; n < b.N; n++ {
-		Send[DummyRequest2, DummyResponse2](ctx, req)
+		res, err = Send[DummyRequest2, DummyResponse2](ctx, req)
+		if err != nil {
+			b.Fatalf("want no err, got %v", err)
+		}
 	}
 }
+
+var err error
+var res DummyResponse2
